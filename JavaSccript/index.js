d93307bfb03a098be76e76f14c9ae9ab06810c23
anushka2031 <anushkatiwari2031@gmail.com>
2025-08-25 21:57:42 +0530

    25Aug

---
+++ JavaSccript/index.js
@@ -0,0 +1,47 @@
+var a=10
+var a=15
+console.log(a)
+
+let b=10
+b=3
+console.log(b)
+
+const c=2
+console.log(c)
+// the constant variable can't be changed or reassigned
+
+console.log(5=='5')
+// compares only the values given not the data type
+
+console.log(s=true)
+//assignment
+
+console.log(5==='5')
+// compares the data type too
+
+console.log({}=={})
+
+// undefied
+var a=null
+
+// null
+//var a=
+
+let d=100
+let to=d==14 
+console.log(d)
+
+let userName="Om"
+let lastName="Tiwari"
+console.log(`${userName}   ${lastName}`)
+
+let e=true
+if(0){
+    console.log("hello")
+}else{
+    console.log("hi")
+}
+
+// ternary operator
+let f=false
+f ? console.log("hii") : console.log("byee")
